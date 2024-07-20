be1fc17e95026d5258e29539dfbaa4e765f71864
leedyun <105830643+leedyun@users.noreply.github.com>
2024-07-20 19:34:58 +0900

    Update and rename calculator.h to calculator.cpp

---
+++ src/calculator.cpp
@@ -0,0 +1,17 @@
+#include "calculator.h"
+
+double Calculator::add(double a, double b) {
+    return a + b;
+}
+
+double Calculator::subtract(double a, double b) {
+    return a - b;
+}
+
+double Calculator::multiply(double a, double b) {
+    return a * b;
+}
+
+double Calculator::divide(double a, double b) {
+    return a / b;
+}
