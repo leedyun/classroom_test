fa0ff7751d0e356bda9527f0d373cc88dfc879d7
leedyun <105830643+leedyun@users.noreply.github.com>
2024-07-20 19:34:30 +0900

    Create caculator.h

---
+++ include/caculator.h
@@ -0,0 +1,12 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+public:
+    double add(double a, double b);
+    double subtract(double a, double b);
+    double multiply(double a, double b);
+    double divide(double a, double b);
+};
+
+#endif // CALCULATOR_H
